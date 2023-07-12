053f48f2ef0d7a81f46744fc67e89a01cc948dc0
aadhivenkat3 <sharmavenkat1@gmail.com>
2023-07-12 12:21:50 +0530

    add

---
+++ main.js
@@ -0,0 +1,16 @@
+// js condition ifelse
+	
+var age=prompt("Enter your age");
+
+if(age>=18){
+	document.write("eligible for vote");
+}
+else{
+	document.write("not eligible for vote");
+}
+
+// Ternary operator (?:)
+
+var age=25;
+
+(age>=18)?"eligible":"not eligible"
